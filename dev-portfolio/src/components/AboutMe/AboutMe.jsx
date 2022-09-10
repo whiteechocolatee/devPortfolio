969c6534f38802b8dc9e20516460c894246e114b
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-10 18:00:16 +0300

    add styles for dark theme

--- dev-portfolio/src/components/AboutMe/AboutMe.jsx
+++ dev-portfolio/src/components/AboutMe/AboutMe.jsx
@@ -7,7 +7,7 @@ import img from "./img/AndreyBlck.png";
 
 export const AboutMe = () => {
   return (
-    <main className={styles.aboutMe}>
+    <main className={`${styles.aboutMe} about-me`}>
       <div className={styles.aboutMeDescription}>
         <h3 className={styles.aboutMeTitle}>
           Hi <Emoji symbol='👋' />,<br />
@@ -21,7 +21,7 @@ export const AboutMe = () => {
         </h3>
       </div>
       <div className={styles.aboutMePhoto}>
-        <div className={styles.photo}>
+        <div className={`${styles.photo} main-photo-bg`}>
           <img
             className='img-fluid'
             src={img}
