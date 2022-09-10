69ba31a454e1516608304029dd9e063e0c9b1ff2
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-10 18:01:10 +0300

    insert all application to theme context and create toggle function for changing state

--- dev-portfolio/src/App.js
+++ dev-portfolio/src/App.js
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
-import { Header } from "./components/Header/Header";
 import "./index.css";
 import { Main } from "./pages/Main";
+import { ThemeContext } from "./Context/ThemeContext";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    setTheme((curr) =>
+      curr === "light" ? "dark" : "light",
+    );
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' exact element={<Main />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeContext.Provider value={{ theme, themeToggler }}>
+      <div id={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' exact element={<Main />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
