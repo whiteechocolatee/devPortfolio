719b47e7937626cead034224b89c3d7b2a9a8bd6
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-10 17:58:12 +0300

    create project card

---
+++ dev-portfolio/src/components/ProjectList/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FiLink, FiGithub } from "react-icons/fi";
+
+import styles from "./projectCard.module.css";
+
+export const ProjectCard = ({
+  title,
+  img,
+  description,
+  preview,
+  code,
+}) => {
+  return (
+    <div className={`${styles.projectCard} project-card`}>
+      <div className={styles.projectImg}>
+        <img src={img} className='img-fluid' alt='' />
+      </div>
+      <div
+        className={`${styles.projectDescription} project-desc`}>
+        <h4>{title}</h4>
+        <p className={styles.projectText}>{description}</p>
+        <div className={`${styles.links} project-links`}>
+          {preview ? (
+            <span>
+              <FiLink />
+              <a
+                href={preview}
+                rel='noreferrer'
+                target='_blank'>
+                Live preview
+              </a>
+            </span>
+          ) : null}
+          <span>
+            <FiGithub />
+            <a href={code} rel='noreferrer' target='_blank'>
+              View code
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
