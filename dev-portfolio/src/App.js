4e512aecb0175fb47c24795ffa7f550c185331ef
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-12 12:05:42 +0300

    insert all new pages

--- dev-portfolio/src/App.js
+++ dev-portfolio/src/App.js
@@ -6,8 +6,12 @@ import {
 } from "react-router-dom";
 
 import "./index.css";
-import { Main } from "./pages/Main";
+import { Main } from "./pages/Main/Main";
 import { ThemeContext } from "./Context/ThemeContext";
+import { About } from "./pages/About/About";
+import { TechStackPage } from "./pages/TechStackPage/TechStackPage";
+import { ProjectsPage } from "./pages/ProjectsPage/ProjectsPage";
+import { ContactUs } from "./pages/ContactUs/ContactUs";
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -24,6 +28,26 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' exact element={<Main />} />
+            <Route
+              path='/about'
+              exact
+              element={<About />}
+            />
+            <Route
+              path='/technologies'
+              exact
+              element={<TechStackPage />}
+            />
+            <Route
+              path='/projects'
+              exact
+              element={<ProjectsPage />}
+            />
+            <Route
+              path='/contacts'
+              exact
+              element={<ContactUs />}
+            />
           </Routes>
         </BrowserRouter>
       </div>
