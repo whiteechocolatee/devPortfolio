2e727bd2293834b33dc07a15c28301a06a4861dd
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-12 12:02:00 +0300

    create experience card component

---
+++ dev-portfolio/src/pages/About/ExperienceCard/ExperienceCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  BiBuildingHouse,
+  BiCalendar,
+} from "react-icons/bi";
+
+import styles from "./experienceCard.module.css";
+
+export const ExperienceCard = ({
+  title,
+  place,
+  workFormat,
+  workDuration,
+}) => {
+  return (
+    <div
+      className={`${styles.experienceCard} experience-card`}>
+      <div className={styles.experienceInfo}>
+        <p className={styles.experienceTitle}>{title}</p>
+        <p className={styles.experiencePlace}>
+          <BiBuildingHouse />
+          <span> {place}</span>
+        </p>
+      </div>
+      <div className={styles.experienceDuration}>
+        <span className={styles.experienceFormat}>
+          {workFormat}
+        </span>
+        <p className={styles.experienceDate}>
+          <BiCalendar /> {workDuration}
+        </p>
+      </div>
+    </div>
+  );
+};
