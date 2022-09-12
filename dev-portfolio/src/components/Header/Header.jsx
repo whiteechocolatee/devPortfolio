c1416ca8ba1dbf47f9ccd0282d85a3cb9b57991f
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-12 12:05:15 +0300

    insert two new components to header

--- dev-portfolio/src/components/Header/Header.jsx
+++ dev-portfolio/src/components/Header/Header.jsx
@@ -1,70 +1,24 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import {
-  AiFillGithub,
-  AiFillLinkedin,
-} from "react-icons/ai";
-import { BsTelegram } from "react-icons/bs";
+
 import ReactSwitch from "react-switch";
 
 import { ThemeContext } from "../../Context/ThemeContext";
 
 import styles from "./header.module.css";
+import { Logo } from "../Logo/Logo";
+import { Navigation } from "../Navigation/Navigation";
+import { SocialNetworks } from "../SocialNetworks/SocialNetworks";
 
 export const Header = () => {
   const { theme, themeToggler } = useContext(ThemeContext);
 
   return (
-    <header>
-      <div className={styles.header}>
-        <div className={styles.title}>
-          <h3>
-            andrey <br />
-            blck
-          </h3>
-        </div>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <Logo />
         <nav className={styles.headerNav}>
-          <ul className={styles.navList}>
-            <li>
-              <NavLink to='#'>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to='#'>About</NavLink>
-            </li>
-            <li>
-              <NavLink to='#'>Tech Stack</NavLink>
-            </li>
-            <li>
-              <NavLink to='#'>Projects</NavLink>
-            </li>
-            <li>
-              <NavLink to='#'>Contact</NavLink>
-            </li>
-            <li className={styles.gitLink}>
-              <a
-                href='https://github.com/whiteechocolatee'
-                rel='noreferrer'
-                target='_blank'>
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className={styles.linkedInLink}>
-              <a
-                href='https://www.linkedin.com/in/whiiteechocolatee/'
-                rel='noreferrer'
-                target='_blank'>
-                <AiFillLinkedin />
-              </a>
-            </li>
-            <li className={styles.telegramLink}>
-              <a
-                href='https://t.me/whiiteechocolatee'
-                rel='noreferrer'
-                target='_blank'>
-                <BsTelegram />
-              </a>
-            </li>
-          </ul>
+          <Navigation />
+          <SocialNetworks />
           <div>
             <ReactSwitch
               checked={theme === "dark"}
