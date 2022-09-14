fa20084d24d431f69430f24d130ff0c0fe8a4d1d
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-14 21:37:19 +0300

    insert into header language switcher

--- dev-portfolio/src/components/Header/Header.jsx
+++ dev-portfolio/src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import ReactSwitch from "react-switch";
-import { useTranslation } from "react-i18next";
-import i18next from "i18next";
 
 import { ThemeContext } from "../../Context/ThemeContext";
 
@@ -9,24 +7,13 @@ import styles from "./header.module.css";
 import { Logo } from "../Logo/Logo";
 import { Navigation } from "../Navigation/Navigation";
 import { SocialNetworks } from "../SocialNetworks/SocialNetworks";
+import { LanguageSwitcher } from "../LanguageSwitcher/LanguageSwitcher";
 
 export const Header = () => {
-  const { i18n, t } = useTranslation(["header"]);
-
-  useEffect(() => {
-    if (localStorage.getItem("i18nextLng")?.length > 2) {
-      i18next.changeLanguage("en");
-    }
-  }, []);
-
-  const handleChangeLanguage = (e) => {
-    i18n.changeLanguage(e.target.value);
-  };
-
   const { theme, themeToggler } = useContext(ThemeContext);
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} container-lg`}>
       <div className={styles.headerContainer}>
         <Logo />
         <nav className={styles.headerNav}>
@@ -38,14 +25,7 @@ export const Header = () => {
               onChange={themeToggler}
             />
           </div>
-          <select
-            className={`${styles.selectLanguage} select`}
-            value={localStorage.getItem("i18nextLng")}
-            onChange={handleChangeLanguage}>
-            <option value='en'>EN</option>
-            <option value='ua'>UA</option>
-            <option value='ru'>RU</option>
-          </select>
+          <LanguageSwitcher />
         </nav>
       </div>
     </header>
