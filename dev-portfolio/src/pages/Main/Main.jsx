972524bd3dd0a4047203f2448fdee73bb9cb5f88
Andrey Chorniy <creatuseandr@icloud.com>
2022-09-15 15:21:06 +0300

    insert banner to main page

--- dev-portfolio/src/pages/Main/Main.jsx
+++ dev-portfolio/src/pages/Main/Main.jsx
@@ -1,56 +1,27 @@
-import React, { useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import React from "react";
+import { motion } from "framer-motion";
 
+import { Banner } from "../../components/Banner/Banner";
 import { AboutMe } from "../../components/AboutMe/AboutMe";
 import { Projects } from "../../components/ProjectList/Projects";
 import { TechStack } from "../../components/TechStack/TechStack";
 import { homeContainerVariants } from "../pageAnimations";
-import styles from "./main.module.css";
 
 export const Main = () => {
-  const { ref, inView } = useInView();
-
-  const animation = useAnimation();
-
-  useEffect(() => {
-    if (inView) {
-      animation.start({
-        x: 0,
-        transition: {
-          type: "spring",
-          duration: 1,
-          bounce: 0.3,
-          delay: 0.3,
-        },
-      });
-    }
-    if (!inView) {
-      animation.start({
-        x: "-100vw",
-      });
-    }
-  }, [animation, inView]);
-
   return (
     <motion.div
       variants={homeContainerVariants}
       initial='hidden'
       animate='visible'
-      exit='exit'
-      className='container-lg'>
-      <AboutMe />
-      <TechStack />
-      <div ref={ref}>
-        <motion.div
-          className={styles.animatedTitle}
-          animate={animation}>
-          <h1>andrey blck</h1>
-          <h1>frontend</h1>
-          <h1>developer</h1>
-        </motion.div>
+      exit='exit'>
+      <div className='container-lg'>
+        <AboutMe />
+        <TechStack />
       </div>
-      <Projects />
+      <Banner />
+      <div className='container-lg'>
+        <Projects />
+      </div>
     </motion.div>
   );
 };
